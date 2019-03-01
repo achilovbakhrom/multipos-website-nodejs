3e4c916a3b77901e13092f32e754c55b43e7e7f8
Фатхулло Бадиров <fathullobadirov@Fathullos-MacBook-Pro.local>
2019-03-01 16:10:20 +0500

    edit profile changes

--- routes/userpage.js
+++ routes/userpage.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('career', {
+router.get('/:lang', function(req, res, next) {
+    res.render('userpage', {
     });
 });
 
